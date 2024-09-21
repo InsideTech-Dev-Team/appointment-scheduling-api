8bc0b72b906744fc73945eff6ecca28e5dd8e33e
Achmad Ilmi Al Akbar <113961961+notilmi@users.noreply.github.com>
2024-09-21 17:42:57 +0700

    feat: Appointment Handler

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 import { Elysia } from "elysia";
+import { HTTPException } from "./utils/types";
+import { appointment } from "./routes/appointment";
 
-const app = new Elysia();
+const app = new Elysia()
+  .onError(({ error }) => {
+    switch (true) {
+      case error instanceof HTTPException:
+        return {
+          status: error.status,
+          message: error.message,
+        };
+      default:
+        return {
+          status: 500,
+          message: "Internal Server Error",
+        };
+    }
+  })
+  .use(appointment);
 
 export type AppointmentScheduleApiType = typeof app;
 export default app;
---
+++ src/routes/appointment.ts
@@ -0,0 +1,88 @@
+import { Elysia, t } from "elysia";
+import { db } from "../utils/db";
+import type { SuccessApiResponse, BaseApiResponse } from "../utils/types";
+
+export const appointment = new Elysia({ prefix: "appointment" })
+  .get("/", async () => {
+    const appointments = await db.appointments.findMany();
+
+    return {
+      message: "Successfully retrieved appointments",
+      data: appointments,
+    } satisfies SuccessApiResponse;
+  })
+  .get(
+    "/:id",
+    async ({ params }) => {
+      const appointment = await db.appointments.findUnique({
+        where: { id: params.id },
+      });
+
+      if (!appointment) {
+        throw new Error("Appointment not found");
+      }
+
+      return {
+        message: "Successfully retrieved appointment",
+        data: appointment,
+      } satisfies SuccessApiResponse;
+    },
+    {}
+  )
+  .post(
+    "/",
+    async ({ body }) => {
+      const appointment = await db.appointments.create({
+        data: {
+          name: body.name,
+          description: body.description,
+        },
+      });
+
+      return {
+        message: "Successfully created appointment",
+        data: appointment,
+      } satisfies SuccessApiResponse;
+    },
+    {
+      body: t.Object({
+        name: t.String(),
+        description: t.String(),
+      }),
+    }
+  )
+  .put(
+    "/:id",
+    async ({ params, body }) => {
+      const appointment = await db.appointments.update({
+        where: { id: params.id },
+        data: {
+          name: body.name,
+          description: body.description,
+        },
+      });
+
+      return {
+        message: "Successfully updated appointment",
+        data: appointment,
+      } satisfies SuccessApiResponse;
+    },
+    {
+      body: t.Object({
+        name: t.String(),
+        description: t.String(),
+        startTime: t.String(),
+      }),
+    }
+  )
+  .delete("/:id", async ({ params }) => {
+    await db.appointments.delete({
+      where: {
+        id: params.id,
+      },
+    });
+
+    return {
+      message: "Successfully Deleted",
+    } satisfies BaseApiResponse;
+  });
